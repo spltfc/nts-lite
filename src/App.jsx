baaee6fd0a1d068c53b338853818c5b4c8c1f456
spltfc <spltfc@gmail.com>
2025-03-19 03:49:07 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useCallback, useState } from 'react'
+import NTSLogo from './NtsLogo'
+import {live, infinite} from './stations'
+
+const LiveBtn = ({isActive, title, streamUrl, onClick}) => {
+    return <div className={`flex h-[120px] items-center justify-center border-1 border-white ${isActive ? 'bg-white text-black' : 'text-white'} cursor-pointer`} onClick={() => onClick(streamUrl)}>
+        <div className="text-3xl font-bold">
+            {title}
+        </div>
+    </div>
+}
+
+const InfiniteBtn = ({isActive, title, description, color, streamUrl, onClick}) => {
+    return <div style={{backgroundColor: color}} className="border-1 border-white">
+        <div className={`flex flex-col h-[150px] gap-[12px] items-center justify-center ${isActive ? 'bg-white text-black' : 'text-white'} cursor-pointer`} onClick={() => onClick(streamUrl)}>
+            <div className="text-xl uppercase font-bold">
+                {title}
+            </div>
+            <div className="text-xs max-w-2/3 text-center font-light">
+                {description}
+            </div>
+        </div>
+    </div>
+}
+
+const Player = ({url}) => {
+    if (!url) return null
+    return <audio controls autoPlay src={url} type="audio/mpeg" className="h-[20px] w-full"></audio>
+}
+
+function App() {
+    const [nowPlaying, setNowPlaying] = useState(null)
+
+    const onStationClick = useCallback((streamUrl) => {
+        if (nowPlaying === streamUrl) {
+            setNowPlaying(null)
+        } else {
+            setNowPlaying(streamUrl)
+        }
+    }, [nowPlaying, setNowPlaying])
+
+    return (
+        <div className="max-w-[800px] mx-auto">
+            <div className="flex items-center justify-center h-[80px]">
+                <Player url={nowPlaying} />
+            </div>
+            <div className="flex gap-[24px] h-[80px] items-center">
+                <div className="w-[80px]">
+                    <NTSLogo />
+                </div>
+                <div className="text-[32px] font-thin italic text-white uppercase">
+                    exxxtra light
+                </div>
+            </div>
+            <div className="grid grid-cols-2">
+                {Object.keys(live).map((stationKey) => {
+                    const {title, streamUrl} = live[stationKey]
+                    return <LiveBtn
+                        key={stationKey}
+                        isActive={nowPlaying === streamUrl}
+                        {...{title, streamUrl}}
+                        onClick={onStationClick}
+                    /> 
+                })}
+            </div>
+            <div className="grid grid-cols-3">
+                {Object.keys(infinite).map((stationKey) => {
+                    const {title, description, color, streamUrl} = infinite[stationKey]
+                    return <InfiniteBtn
+                        key={stationKey}
+                        isActive={nowPlaying === streamUrl}
+                        onClick={onStationClick}
+                        {...{title, description, color, streamUrl}}
+                    />
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/stations.js
@@ -0,0 +1,112 @@
+export const MIXTAPE_ORIGIN = "https://stream-mixtape-geo.ntslive.net"
+export const RADIO_BASE_URL = "https://stream-relay-geo.ntslive.net"
+
+export const live = {
+    'live1': {
+        title: "Live 1",
+        streamUrl: RADIO_BASE_URL + "/stream"
+    },
+    'live2': {
+        title: "Live 2",
+        streamUrl: RADIO_BASE_URL + "/stream2"
+    },
+}
+
+export const infinite = {
+    poolside: {
+        title: "Poolside",
+        color: '#60556E',
+        description: 'Balearic, boogie, and sophisti-pop for poolsides, beaches and car stereos.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape4"
+    },
+    "slow-focus": {
+        title: "Slow Focus",
+        color: '#795772',
+        description: 'Meditative, relaxing and beatless: ambient, drone and ragas.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape"
+    },
+    "100-percent-hip-hop": {
+        title: "Low Key",
+        color: '#7B5F6A',
+        description: 'Keeping it simple with lo-fi hip-hop and smooth R’n’B.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape2"
+    },
+    "memory-lane": {
+        title: "Memory Lane",
+        color: '#659051',
+        description: 'Turn on, tune in, drop out.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape6"
+    },
+    "4-to-the-floor": {
+        title: "4 To The Floor",
+        color: '#745F3A',
+        description: 'House and techno from past to present.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape5"
+    },
+    "island-time": {
+        title: "Island Time",
+        color: '#747B5F',
+        description: 'Easy skanking - reggae, dub, and plenty more.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape21"
+    },
+    "the-tube": {
+        title: "The Tube",
+        color: '#639DA7',
+        description: 'Oddball post-punk, industrial provocation, and minimal wave.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape26"
+    },
+    "sheet-music": {
+        title: "Sheet Music",
+        color: '#1F1C18',
+        description: 'The best of classical and contemporary composition.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape35"
+    },
+    feelings: {
+        title: "Feelings",
+        color: '#3A222C',
+        description: 'Sweet soul, gospel, boogie, and beyond.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape27"
+    },
+    expansions: {
+        title: "Expansions",
+        color: '#764133',
+        description: 'Jazz and its many mind-expanding variations.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape3"
+    },
+    "rap-house": {
+        title: "Rap House",
+        color: '#91538A',
+        description: '808s and champagne.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape22"
+    },
+    labyrinth: {
+        title: "Labyrinth",
+        color: '#726C28',
+        description: 'Enter the void.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape31"
+    },
+    sweat: {
+        title: "Sweat",
+        color: '#483315',
+        description: 'A new wave of international party music.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape24"
+    },
+    otaku: {
+        title: "Otaku",
+        color: '#996366',
+        description: 'Video game and anime soundtracks, for fanboys and fangirls.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape36"
+    },
+    "the-pit": {
+        title: "The Pit",
+        color: '#727272',
+        description: 'Behold the songs of the ancient metal bards.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape34"
+    },
+    "field-recordings": {
+        title: "Field Recordings",
+        color: '#6B5E4D',
+        description: 'Natural ambience from NTS listeners around the world.',
+        streamUrl: MIXTAPE_ORIGIN + "/mixtape23"
+    }
+}
